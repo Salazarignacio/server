c496217890d6941c5b8743e9c7040ad25e25a934
ignacio salazar <ignaciosalazar986@gmail.com>
2024-04-30 14:39:49 -0300

    population

--- src/data/mongo/models/cart.model.js
+++ src/data/mongo/models/cart.model.js
@@ -1,17 +1,20 @@
-import { Schema, model } from "mongoose";
-
+import { Schema, model, Types } from "mongoose";
 
 const collection = "carts";
 
 const schema = new Schema(
   {
-    user_id: { type: String, required: true },
-    product_id: { type: String, required: true },
+    user_id: { type: Types.ObjectId, required: true, ref: "users" },
+    product_id: { type: Types.ObjectId, required: true, ref: "products" },
     quantity: { type: Number, default: 1 },
     state: { type: String },
   },
   { timestamps: true }
 );
+
+schema.pre("find", function () {
+  this.populate("user_id", "email");
+});
 
 const Cart = model(collection, schema);
 export default Cart;
