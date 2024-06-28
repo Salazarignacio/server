0d49573a724bdf4fc9c8e539b4548cc9624b4a25
Ignacio <ignaciosalazar986@gmail.com>
2024-06-28 20:07:17 -0300

    verify loop

--- src/mocks/updateUsers.js
+++ src/mocks/updateUsers.js
@@ -2,18 +2,18 @@ import usersRepository from "../repositories/users.rep.js";
 import environment from "../../utils/env.util.js";
 import dbConnect from "../utils/dbConect.util.js";
 
+
 async function updateData() {
   dbConnect();
   try {
     let data = await usersRepository.readRepository();
-    
-    
-    for (let i = 0; i<data.length; i++){
-    const uu =  await usersRepository.updateRepository(data[i]._id, { age: 31 });
-}
+
+    for (let i = 0; i < data.length; i++) {
+      await usersRepository.updateRepository(data[i]._id, { verify: true });
+    }
     console.log(data);
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 
