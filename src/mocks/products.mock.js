98672f4b9bd62c836937240458e4ab0f039a4a57
Ignacio <ignaciosalazar986@gmail.com>
2024-06-29 12:04:16 -0300

    mocks

--- src/mocks/products.mock.js
+++ src/mocks/products.mock.js
@@ -1,18 +1,21 @@
 import dbConnect from "../utils/dbConect.util.js";
-import environment from "../../utils/env.util.js";
+import "../../utils/env.util.js";
 import productsRepository from "../repositories/products.rep.js";
 import { faker } from "@faker-js/faker";
 
 async function createData() {
   dbConnect();
+  const category = ["shoes", "shirts", "socks", "pants"];
+
   try {
     const product = {
       title: faker.commerce.product(),
+      category: category[Math.floor(Math.random() * 4)],
+      photo: faker.image.avatar(),
       price: Math.floor(Math.random() * 101),
-      stock: 13
+      stock: Math.floor(Math.random() * 100001),
     };
-    /* await productsRepository.createRepository(product); */
-    console.log(environment.MONGO_URI);
+    await productsRepository.createRepository(product);
   } catch (error) {
     console.log(error);
   }
