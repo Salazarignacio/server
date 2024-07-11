a24c1e2cc7846f853f58c017e7c64764b1a3b2d3
ignacio salazar <ignaciosalazar986@gmail.com>
2024-07-11 16:12:49 -0300

    lp

--- utils/winston.prod.js
+++ utils/winston.prod.js
@@ -6,7 +6,7 @@ const levels = { FATAL: 0, ERROR: 1, INFO: 2, HTTP: 3 };
 const colors = { FATAL: "red", ERROR: "yellow", INFO: "blue", HTTP: "white" };
 addColors(colors);
 
-const logger = createLogger({
+const loggerProd = createLogger({
   levels,
   format: colorize(),
   transports: [
