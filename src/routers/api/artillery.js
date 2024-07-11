d298685209a782314a61969457a18c658cd5e077
ignacio salazar <ignaciosalazar986@gmail.com>
2024-07-11 17:19:58 -0300

    logs

--- src/routers/api/artillery.js
+++ src/routers/api/artillery.js
@@ -1,33 +1,19 @@
 import CustomRouter from "./CustomRouter.js";
+import logger from "../../../utils/winston.utils.js";
 
-class ArtilleryRouter extends CustomRouter {
+class LoggerRouter extends CustomRouter {
   init() {
-    this.read("/simplex", ["PUBLIC"], (req, res, next) => {
-      try {
-        let total = 1;
-        for (let i = 0; i < 100; i++) {
-          total = i * i;
-        }
-        return res.send({ total });
-      } catch (error) {
-        return next(error);
-      }
+    this.read("/", ["PUBLIC"], (req, res) => {
+      req.logger = logger; // Asegúrate de que req.logger esté disponible
+      req.logger.FATAL("Este es un mensaje de log FATAL");
+      req.logger.ERROR("Este es un mensaje de log ERROR");
+      req.logger.INFO("Este es un mensaje de log INFO");
+      req.logger.HTTP("Este es un mensaje de log HTTP");
+
+      res.send("LOGS");
     });
-    this.read("/complex", ["PUBLIC"], (req, res, next) => {
-      try {
-        let total = 1;
-        for (let i = 0; i < 1000000000; i++) {
-          total = i * i;
-        }
-        return res.send({ total });
-      } catch (error) {
-        return next(error);
-      }
-    });
-    this.read("/", ["PUBLIC"], (req,res,next)=>{
-      return res.send({ response: req})
-    })
   }
 }
-const artillery = new ArtilleryRouter();
+
+const artillery = new LoggerRouter();
 export default artillery.getRouter();
