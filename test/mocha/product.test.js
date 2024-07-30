92c0a8ca24d95a7fcc1abdd4050b4e19acf66cdc
Ignacio <ignaciosalazar986@gmail.com>
2024-07-30 11:51:04 -0300

    mocha

--- test/mocha/product.test.js
+++ test/mocha/product.test.js
@@ -1,21 +1,16 @@
 import assert from "assert";
+import environment from '../../utils/env.util.js'
 import dao from "../../src/data/dao.factory.js";
-import environment from "../../utils/env.util.js";
-import dbConnect from "../../src/utils/dbConect.util.js";
-const {productsManagerMongo} = dao
+const { productsManagerMongo } = dao;
 
-dbConnect()
-
-describe(
-    'Testing product', ()=>{
-        const data = {
-            
-            title: "ASSERT PRODUCT",
-            photo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3UPfGlTCdWJBSMxh5wKzmtp1uZ8fFUeek6Q&usqp=CAU",
-            category: "boots",
-            price: 15,
-            stock: 40000
-          }
-        it('Testeando que data el objeto tenga un title', assert.ok(data.title))
-    }
-)
+describe("Testing product", () => {
+  const data = {
+    title: "ASSERT PRODUCT",
+    photo:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3UPfGlTCdWJBSMxh5wKzmtp1uZ8fFUeek6Q&usqp=CAU",
+    category: "boots",
+    price: 15,
+    stock: 40000,
+  };
+  it("Testeando que el produto reciba un objeto con la prop title", ()=> {assert.ok(data.title)});
+});
