e60a2b5c9e276e2de128e39cc142a7e9428e35e4
Ignacio <ignaciosalazar986@gmail.com>
2024-08-13 12:20:55 -0300

    half

--- utils/updateProducts.js
+++ utils/updateProducts.js
@@ -7,9 +7,13 @@ async function updateProduct() {
   try {
     let data = await productsRepository.readRepository();
 
-    for (let i = 0; i < data.length; i++) {
-      await productsRepository.updateRepository(data[i]._id, { supplier_id: {"_id":"66b5074b80880b63e6fae2f6"} });
+    for (let i = 0; i < data.length/2; i++) {
+      await productsRepository.updateRepository(data[i]._id, { supplier_id: {"_id":"667b3772802ca3bea0430100"} });
     }
+
+     for (let i = data.length-1; i > data.length/2; i--) {
+      await productsRepository.updateRepository(data[i]._id, { supplier_id: {"_id":"66b5074b80880b63e6fae2f6"} });
+    } 
   } catch (error) {
     throw error;
   }
