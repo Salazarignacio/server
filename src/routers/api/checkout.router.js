60c7597ad2f014e23c9a5a04cc3de45209c4ebbd
Ignacio <ignaciosalazar986@gmail.com>
2024-09-04 17:53:01 -0300

    c

--- src/routers/api/checkout.router.js
+++ src/routers/api/checkout.router.js
@@ -2,7 +2,11 @@ import { Router } from "express";
 import checkout from "../../controllers/checkout.controller.js";
 
 const checkoutRouter = Router();
+async function che() {
+  return { saludo: "hola amigo" };
+}
 
 checkoutRouter.post("/checkout", checkout);
+checkoutRouter.get("/che", che);
 
-export default checkoutRouter;
+export { checkoutRouter };
